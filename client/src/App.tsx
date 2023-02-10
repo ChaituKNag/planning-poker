082620560bd5011893e1ce6448c112c42279f254
Naga Chaitanya Konada <13428505+ChaituKNag@users.noreply.github.com>
2023-02-10 15:32:16 +0000

    feat: client server integration wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client'
 
 function App() {
+  useEffect(() => {
+    const socket = io('http://172.16.5.4:4000')
+    socket.on('connect', () => {
+      console.log('Connected to server')
+    })
+  }, [])
   return (
     <div className="App">
       Planning Poker
